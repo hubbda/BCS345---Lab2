ba872d4b3d4a3f8bcd2dc83738c5f7f07e96eac6
hubbda <hubbda@farmingdale.edu>
2022-02-09 12:10:06 -0500

    Did part 2 for lab2

--- src/lab2.java
+++ src/lab2.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class lab2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //Part 1
         Scanner keyboard = new Scanner(System.in);
 
         char standing;
@@ -35,6 +40,55 @@ public class lab2 {
                 dues = 0;
         }
 
-        System.out.printf("Dues: %.2f", dues);
+        System.out.printf("Dues: %.2f\n", dues);
+
+        //Part 2
+
+        int lower;
+        int upper;
+
+        System.out.print("Enter the lower-bound integer: ");
+        lower = keyboard.nextInt();
+
+        System.out.print("Enter the upper-bound integer: ");
+        upper = keyboard.nextInt();
+
+        while(lower > upper) {
+            System.out.println("invalid values");
+
+            System.out.print("Enter the lower-bound integer: ");
+            lower = keyboard.nextInt();
+
+            System.out.print("Enter the upper-bound integer: ");
+            upper = keyboard.nextInt();
+        }
+
+        File file = new File("lab2data.txt");
+        PrintWriter outfile = new PrintWriter(file);
+
+        for(int i = lower; i <= upper; i++) {
+            if(i % 11 == 0) {
+                System.out.print(i + ",");
+                outfile.print(i + ",");
+            }
+        }
+        System.out.println("");
+        outfile.close();
+
+        Scanner infile = new Scanner(file);
+        int num;
+        int sum = 0;
+        int count = 0;
+        double average;
+        infile.useDelimiter(",");
+        while (infile.hasNext()) {
+            num = infile.nextInt();
+            sum += num;
+            count++;
+            System.out.print(num + " ");
+        }
+        average = (double)sum / count;
+        System.out.printf("\n\nThe sum is %d and the average is %.2f\n", sum, average);
+        infile.close();
     }
 }
